fb2720893d74db71ae25f99e0a6667cebb150f00
yassin29 <ynhassan22@gmail.com>
2023-02-22 09:30:06 +0100

    Create html CSS js files and Add createGrid function

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container")
+
+const createGrid = (amtOfGrids) => {
+    for (let i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement('div')
+        row.classList.add('grid-row')
+
+        for (let j = 0; j < amtOfGrids; j++) {
+            const gridBox = document.createElement('div')
+            gridBox.classList.add('grid-box')
+            row.appendChild(gridBox)
+        }
+
+        container.appendChild(row)
+    }
+}
