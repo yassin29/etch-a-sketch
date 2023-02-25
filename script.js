49ef42780674600c054e492916afdbebe2ebcb2a
yassin29 <ynhassan22@gmail.com>
2023-02-25 13:13:40 +0100

    Add reset button and functionality to avoid grids stacking on top with new prompts

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const container = document.querySelector(".container")
+const resetButton = document.querySelector("button")
 
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("wrapper")
+
     for (let i = 0; i < amtOfGrids; i++) {
         const row = document.createElement('div')
         row.classList.add('grid-row')
 
         for (let j = 0; j < amtOfGrids; j++) {
+            const widthAndHeight = 960 / amtOfGrids
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
+            gridBox.style.width = `${widthAndHeight}px`
+            gridBox.style.height = `${widthAndHeight}px`
             
             gridBox.addEventListener("mouseenter", () => {
                 gridBox.style.backgroundColor = "black"
@@ -16,9 +23,27 @@ const createGrid = (amtOfGrids) => {
             row.appendChild(gridBox)
         }
 
-        container.appendChild(row)
+        wrapper.appendChild(row)
     }
+    
+    container.appendChild(wrapper)
 }
 
-const sizeOfGrid = 20
-createGrid(sizeOfGrid)
+resetButton.addEventListener("click", () => {
+    let userSize = Number(prompt("Choose dimensions between 0 and 100"))
+
+    while(userSize > 100)
+    userSize = Number(prompt("Pick a number that is 100 or less"))
+
+    const wrapper = document.querySelector(".wrapper")
+
+    if (!wrapper) {
+        createGrid(userSize)
+
+    } else {
+    wrapper.remove()
+    createGrid(userSize)
+    }
+})
+
+
